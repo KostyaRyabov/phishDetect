bb1e91343a94d4c754f685d89e4cfa88b58edd5c
Ryabov K <KostyaRyabov@users.noreply.github.com>
2021-01-17 18:32:34 +0300

    - Phishing hint words were added. - Generate legitimate urls by ranked domains (badly)

---
+++ feature_extractor.py
@@ -0,0 +1,448 @@
+from langdetect import detect
+from googletrans import Translator
+from console_progressbar import ProgressBar
+from datetime import date
+import pandas
+import tldextract
+import requests
+import json
+import csv
+import os
+import re
+
+from urllib.parse import urlparse, urlsplit
+from bs4 import BeautifulSoup
+
+key = open("OPR_key.txt").read()
+
+translator = Translator()
+
+
+def load_phishHints():
+    hints_dir = "data/phish_hints/"
+    file_list = os.listdir(hints_dir)
+
+    if file_list:
+        return [{leng[0:2]: pandas.read_csv(hints_dir + leng, header=None)[0].tolist()} for leng in file_list][0]
+    else:
+        hints = {'en': [
+            'login',
+            'logon',
+            'authorization',
+            'registration',
+            'user',
+            'password',
+            'name',
+            'nickname',
+            'profile',
+            'key',
+            'phone',
+            'mail',
+            'bank',
+            'card',
+            'pincode',
+            'enter',
+            'visa',
+            'cvv',
+            'cvp',
+            'cvc',
+            'ccv',
+            'pass',
+            'reg',
+            'log',
+            'auth',
+            'psw'
+        ]
+        }
+
+        data = pandas.DataFrame(hints)
+        filename = "data/phish_hints/en.csv"
+        data.to_csv(filename, index=False, header=False)
+
+        return hints
+
+
+phish_hints = load_phishHints()
+
+
+def check_Language(text):
+    language = translator.detect(text).lang
+
+    if language not in phish_hints:
+        phish_hints[language] = translator.translate(";".join(phish_hints['en'][0:16]), src='en',
+                                                     dest=language).text.split(";")
+        data = pandas.DataFrame(phish_hints[language])
+        filename = "data/phish_hints/{0}.csv".format(language)
+        data.to_csv(filename, index=False, header=False)
+
+    return language
+
+
+def is_URL_accessible(url):
+    page = None
+    try:
+        page = requests.get(url, timeout=5)
+    except:
+        parsed = urlparse(url)
+        url = parsed.scheme + '://' + parsed.netloc
+        if not parsed.netloc.startswith('www'):
+            url = parsed.scheme + '://www.' + parsed.netloc
+            try:
+                page = requests.get(url, timeout=5)
+            except:
+                page = None
+                pass
+
+    if page and page.status_code == 200 and page.content not in ["b''", "b' '"]:
+        return True, url, page
+    else:
+        return False, None, None
+
+
+def get_domain(url):
+    o = urlsplit(url)
+    return o.hostname, tldextract.extract(url).domain, o.path
+
+
+def extract_data_from_URL(hostname, content, domain, Href, Link, Anchor, Media, Form, CSS, Favicon, IFrame, Title,
+                          Text):
+    Null_format = ["", "#", "#nothing", "#doesnotexist", "#null", "#void", "#whatever",
+                   "#content", "javascript::void(0)", "javascript::void(0);", "javascript::;", "javascript"]
+
+    soup = BeautifulSoup(content, 'html.parser', from_encoding='iso-8859-1')
+
+    # collect all external and internal hrefs from url
+    for href in soup.find_all('a', href=True):
+        dots = [x.start(0) for x in re.finditer('\.', href['href'])]
+        if hostname in href['href'] or domain in href['href'] or len(dots) == 1 or not href['href'].startswith('http'):
+            if "#" in href['href'] or "javascript" in href['href'].lower() or "mailto" in href['href'].lower():
+                Anchor['unsafe'].append(href['href'])
+            if not href['href'].startswith('http'):
+                if not href['href'].startswith('/'):
+                    Href['internals'].append(hostname + '/' + href['href'])
+                elif href['href'] in Null_format:
+                    Href['null'].append(href['href'])
+                else:
+                    Href['internals'].append(hostname + href['href'])
+        else:
+            Href['externals'].append(href['href'])
+            Anchor['safe'].append(href['href'])
+
+    # collect all media src tags
+    for img in soup.find_all('img', src=True):
+        dots = [x.start(0) for x in re.finditer('\.', img['src'])]
+        if hostname in img['src'] or domain in img['src'] or len(dots) == 1 or not img['src'].startswith('http'):
+            if not img['src'].startswith('http'):
+                if not img['src'].startswith('/'):
+                    Media['internals'].append(hostname + '/' + img['src'])
+                elif img['src'] in Null_format:
+                    Media['null'].append(img['src'])
+                else:
+                    Media['internals'].append(hostname + img['src'])
+        else:
+            Media['externals'].append(img['src'])
+
+    for audio in soup.find_all('audio', src=True):
+        dots = [x.start(0) for x in re.finditer('\.', audio['src'])]
+        if hostname in audio['src'] or domain in audio['src'] or len(dots) == 1 or not audio['src'].startswith('http'):
+            if not audio['src'].startswith('http'):
+                if not audio['src'].startswith('/'):
+                    Media['internals'].append(hostname + '/' + audio['src'])
+                elif audio['src'] in Null_format:
+                    Media['null'].append(audio['src'])
+                else:
+                    Media['internals'].append(hostname + audio['src'])
+        else:
+            Media['externals'].append(audio['src'])
+
+    for embed in soup.find_all('embed', src=True):
+        dots = [x.start(0) for x in re.finditer('\.', embed['src'])]
+        if hostname in embed['src'] or domain in embed['src'] or len(dots) == 1 or not embed['src'].startswith('http'):
+            if not embed['src'].startswith('http'):
+                if not embed['src'].startswith('/'):
+                    Media['internals'].append(hostname + '/' + embed['src'])
+                elif embed['src'] in Null_format:
+                    Media['null'].append(embed['src'])
+                else:
+                    Media['internals'].append(hostname + embed['src'])
+        else:
+            Media['externals'].append(embed['src'])
+
+    for i_frame in soup.find_all('iframe', src=True):
+        dots = [x.start(0) for x in re.finditer('\.', i_frame['src'])]
+        if hostname in i_frame['src'] or domain in i_frame['src'] or len(dots) == 1 or not i_frame['src'].startswith(
+                'http'):
+            if not i_frame['src'].startswith('http'):
+                if not i_frame['src'].startswith('/'):
+                    Media['internals'].append(hostname + '/' + i_frame['src'])
+                elif i_frame['src'] in Null_format:
+                    Media['null'].append(i_frame['src'])
+                else:
+                    Media['internals'].append(hostname + i_frame['src'])
+        else:
+            Media['externals'].append(i_frame['src'])
+
+    # collect all link tags
+    for link in soup.findAll('link', href=True):
+        dots = [x.start(0) for x in re.finditer('\.', link['href'])]
+        if hostname in link['href'] or domain in link['href'] or len(dots) == 1 or not link['href'].startswith('http'):
+            if not link['href'].startswith('http'):
+                if not link['href'].startswith('/'):
+                    Link['internals'].append(hostname + '/' + link['href'])
+                elif link['href'] in Null_format:
+                    Link['null'].append(link['href'])
+                else:
+                    Link['internals'].append(hostname + link['href'])
+        else:
+            Link['externals'].append(link['href'])
+
+    for script in soup.find_all('script', src=True):
+        dots = [x.start(0) for x in re.finditer('\.', script['src'])]
+        if hostname in script['src'] or domain in script['src'] or len(dots) == 1 or not script['src'].startswith(
+                'http'):
+            if not script['src'].startswith('http'):
+                if not script['src'].startswith('/'):
+                    Link['internals'].append(hostname + '/' + script['src'])
+                elif script['src'] in Null_format:
+                    Link['null'].append(script['src'])
+                else:
+                    Link['internals'].append(hostname + script['src'])
+        else:
+            Link['externals'].append(link['href'])
+
+    # collect all css
+    for link in soup.find_all('link', rel='stylesheet'):
+        dots = [x.start(0) for x in re.finditer('\.', link['href'])]
+        if hostname in link['href'] or domain in link['href'] or len(dots) == 1 or not link['href'].startswith('http'):
+            if not link['href'].startswith('http'):
+                if not link['href'].startswith('/'):
+                    CSS['internals'].append(hostname + '/' + link['href'])
+                elif link['href'] in Null_format:
+                    CSS['null'].append(link['href'])
+                else:
+                    CSS['internals'].append(hostname + link['href'])
+        else:
+            CSS['externals'].append(link['href'])
+
+    for style in soup.find_all('style', type='text/css'):
+        try:
+            start = str(style[0]).index('@import url(')
+            end = str(style[0]).index(')')
+            css = str(style[0])[start + 12:end]
+            dots = [x.start(0) for x in re.finditer('\.', css)]
+            if hostname in css or domain in css or len(dots) == 1 or not css.startswith('http'):
+                if not css.startswith('http'):
+                    if not css.startswith('/'):
+                        CSS['internals'].append(hostname + '/' + css)
+                    elif css in Null_format:
+                        CSS['null'].append(css)
+                    else:
+                        CSS['internals'].append(hostname + css)
+            else:
+                CSS['externals'].append(css)
+        except:
+            continue
+
+    # collect all form actions
+    for form in soup.findAll('form', action=True):
+        dots = [x.start(0) for x in re.finditer('\.', form['action'])]
+        if hostname in form['action'] or domain in form['action'] or len(dots) == 1 or not form['action'].startswith(
+                'http'):
+            if not form['action'].startswith('http'):
+                if not form['action'].startswith('/'):
+                    Form['internals'].append(hostname + '/' + form['action'])
+                elif form['action'] in Null_format or form['action'] == 'about:blank':
+                    Form['null'].append(form['action'])
+                else:
+                    Form['internals'].append(hostname + form['action'])
+        else:
+            Form['externals'].append(form['action'])
+
+    # collect all link tags
+    for head in soup.find_all('head'):
+        for head.link in soup.find_all('link', href=True):
+            dots = [x.start(0) for x in re.finditer('\.', head.link['href'])]
+            if hostname in head.link['href'] or len(dots) == 1 or domain in head.link['href'] or not head.link[
+                'href'].startswith('http'):
+                if not head.link['href'].startswith('http'):
+                    if not head.link['href'].startswith('/'):
+                        Favicon['internals'].append(hostname + '/' + head.link['href'])
+                    elif head.link['href'] in Null_format:
+                        Favicon['null'].append(head.link['href'])
+                    else:
+                        Favicon['internals'].append(hostname + head.link['href'])
+            else:
+                Favicon['externals'].append(head.link['href'])
+
+        for head.link in soup.findAll('link', {'href': True, 'rel': True}):
+            isicon = False
+            if isinstance(head.link['rel'], list):
+                for e_rel in head.link['rel']:
+                    if (e_rel.endswith('icon')):
+                        isicon = True
+            else:
+                if (head.link['rel'].endswith('icon')):
+                    isicon = True
+
+            if isicon:
+                dots = [x.start(0) for x in re.finditer('\.', head.link['href'])]
+                if hostname in head.link['href'] or len(dots) == 1 or domain in head.link['href'] or not head.link[
+                    'href'].startswith('http'):
+                    if not head.link['href'].startswith('http'):
+                        if not head.link['href'].startswith('/'):
+                            Favicon['internals'].append(hostname + '/' + head.link['href'])
+                        elif head.link['href'] in Null_format:
+                            Favicon['null'].append(head.link['href'])
+                        else:
+                            Favicon['internals'].append(hostname + head.link['href'])
+                else:
+                    Favicon['externals'].append(head.link['href'])
+
+    # collect i_frame
+    for i_frame in soup.find_all('iframe', width=True, height=True, frameborder=True):
+        if i_frame['width'] == "0" and i_frame['height'] == "0" and i_frame['frameborder'] == "0":
+            IFrame['invisible'].append(i_frame)
+        else:
+            IFrame['visible'].append(i_frame)
+    for i_frame in soup.find_all('iframe', width=True, height=True, border=True):
+        if i_frame['width'] == "0" and i_frame['height'] == "0" and i_frame['border'] == "0":
+            IFrame['invisible'].append(i_frame)
+        else:
+            IFrame['visible'].append(i_frame)
+    for i_frame in soup.find_all('iframe', width=True, height=True, style=True):
+        if i_frame['width'] == "0" and i_frame['height'] == "0" and i_frame['style'] == "border:none;":
+            IFrame['invisible'].append(i_frame)
+        else:
+            IFrame['visible'].append(i_frame)
+
+    # get page title
+    try:
+        Title = soup.title.string
+    except:
+        pass
+
+    # get content text
+    Text = soup.get_text()
+
+    return Href, Link, Anchor, Media, Form, CSS, Favicon, IFrame, Title, Text
+
+
+def generate_legitimate_urls(N):
+    pb = ProgressBar(total=N, prefix='generate legitimate urls', decimals=2, length=50, fill='█',
+                     zfill='-')
+
+    domain_list = pandas.read_csv("data/ranked_domains/14-1-2021.csv",
+                                  header=None)[1].tolist()
+
+    url_list = []
+
+    for domain in domain_list:
+        url = search_for_vulnerable_URLs(domain)
+
+        if url:
+            url_list.append(url)
+            pb.print_progress_bar(len(url_list))
+
+            if len(url_list) >= N:
+                break
+
+    pandas.DataFrame(url_list).to_csv("data/urls/legitimate/{0}.csv".format(date.today().strftime("%d-%m-%Y")), index=False, header=False)
+
+
+def search_for_vulnerable_URLs(domain):
+    url = 'http://' + domain
+
+    Href = {'internals': [], 'externals': [], 'null': []}
+    Link = {'internals': [], 'externals': [], 'null': []}
+    Anchor = {'safe': [], 'unsafe': [], 'null': []}
+    Media = {'internals': [], 'externals': [], 'null': []}
+    Form = {'internals': [], 'externals': [], 'null': []}
+    CSS = {'internals': [], 'externals': [], 'null': []}
+    Favicon = {'internals': [], 'externals': [], 'null': []}
+    IFrame = {'visible': [], 'invisible': [], 'null': []}
+    Title = ''
+    Text = ''
+    state, url, page = is_URL_accessible(url)
+
+    if state:
+        content = page.content
+        hostname, domain, path = get_domain(url)
+        extracted_domain = tldextract.extract(url)
+        domain = extracted_domain.domain + '.' + extracted_domain.suffix
+
+        Href, Link, Anchor, Media, Form, CSS, Favicon, IFrame, Title, Text = extract_data_from_URL(hostname, content,
+                                                                                                   domain, Href, Link,
+                                                                                                   Anchor, Media, Form,
+                                                                                                   CSS, Favicon, IFrame,
+                                                                                                   Title, Text)
+
+        for hint in phish_hints['en']:
+            for aURL in Href['internals'] + Link['internals']:
+                if hint in aURL.lower():
+                    state, aaURL, page = is_URL_accessible('http://'+aURL)
+                    if state:
+                        return aaURL
+
+            for aURL in Href['externals'] + Link['externals']:
+                if hint in aURL.lower():
+                    state, aaURL, page = is_URL_accessible(aURL)
+                    if state:
+                        return aaURL
+
+    else:
+        url = None
+
+    return url
+
+
+def extract_features(url):
+    def words_raw_extraction(domain, subdomain, path):
+        w_domain = re.split("\-|\.|\/|\?|\=|\@|\&|\%|\:|\_", domain.lower())
+        w_subdomain = re.split("\-|\.|\/|\?|\=|\@|\&|\%|\:|\_", subdomain.lower())
+        w_path = re.split("\-|\.|\/|\?|\=|\@|\&|\%|\:|\_", path.lower())
+        raw_words = w_domain + w_path + w_subdomain
+        w_host = w_domain + w_subdomain
+        raw_words = list(filter(None, raw_words))
+        return raw_words, list(filter(None, w_host)), list(filter(None, w_path))
+
+    Href = {'internals': [], 'externals': [], 'null': []}
+    Link = {'internals': [], 'externals': [], 'null': []}
+    Anchor = {'safe': [], 'unsafe': [], 'null': []}
+    Media = {'internals': [], 'externals': [], 'null': []}
+    Form = {'internals': [], 'externals': [], 'null': []}
+    CSS = {'internals': [], 'externals': [], 'null': []}
+    Favicon = {'internals': [], 'externals': [], 'null': []}
+    IFrame = {'visible': [], 'invisible': [], 'null': []}
+    Title = ''
+    Text = ''
+    state, url, page = is_URL_accessible(url)
+    if state:
+        content = page.content
+        hostname, domain, path = get_domain(url)
+        extracted_domain = tldextract.extract(url)
+        domain = extracted_domain.domain + '.' + extracted_domain.suffix
+        subdomain = extracted_domain.subdomain
+        tmp = url[url.find(extracted_domain.suffix):len(url)]
+        pth = tmp.partition("/")
+        path = pth[1] + pth[2]
+        words_raw, words_raw_host, words_raw_path = words_raw_extraction(extracted_domain.domain, subdomain, pth[2])
+        tld = extracted_domain.suffix
+        parsed = urlparse(url)
+        scheme = parsed.scheme
+
+        Href, Link, Anchor, Media, Form, CSS, Favicon, IFrame, Title, Text = extract_data_from_URL(hostname, content,
+                                                                                                   domain, Href, Link,
+                                                                                                   Anchor, Media, Form,
+                                                                                                   CSS, Favicon, IFrame,
+                                                                                                   Title, Text)
+
+        for hint in phish_hints['en']:
+            for href in Href['internals'] + Href['externals']:
+                if hint in href.lower():
+                    print(href, hint)
+
+            for lnk in Link['internals'] + Link['externals']:
+                if hint in lnk.lower():
+                    print(lnk, hint)
+
+    return None
--- main.py
+++ main.py
@@ -1,41 +1,38 @@
 import requests
 import pandas
 import io
-from datetime import date
+import os
 
+import feature_extractor as fe
 
 legitimate_url_list = []
 phish_url_list = []
 brand_list = []
 
 
-def loadDatasets():
+def loadRawData():
     global brand_list, legitimate_url_list, phish_url_list
 
-    brand_list = pandas.read_csv("datasets/brands/brandirectory-ranking-data-global-2020.csv", usecols=['Brand'])[
+    brand_list = pandas.read_csv("data/brands/brandirectory-ranking-data-global-2020.csv", usecols=['Brand'])[
         "Brand"].tolist()
 
-    legitimate_url_list = pandas.read_csv("datasets/legitimate/top-websites-1m (14-1-2021).csv", usecols=[1],
-                                          header=None)[1].tolist()
+    leg_urls_path = "data/urls/legitimate/"
+    legitimate_url_list = pandas.read_csv(leg_urls_path+os.listdir(leg_urls_path)[0],
+                                          header=None)[0].tolist()
 
-    try:
-        http_request = requests.get('http://data.phishtank.com/data/online-valid.csv')
-        phish_url_list = pandas.read_csv(io.StringIO(http_request.content.decode('utf-8')), usecols=['url'])
-        filename = "datasets/phish/{0}.csv".format(date.today().strftime("%d-%m-%Y"))
-        phish_url_list.to_csv(filename, index=False, header=False)
-        phish_url_list = phish_url_list['url'].tolist()
-    except requests.exceptions.RequestException as e:
-        raise SystemExit(e)
+    # try:
+    #     http_request = requests.get('http://data.phishtank.com/data/online-valid.csv')
+    #     phish_url_list = pandas.read_csv(io.StringIO(http_request.content.decode('utf-8')), usecols=['url'])
+    #     filename = "data/urls/phish/{0}.csv".format(date.today().strftime("%d-%m-%Y"))
+    #     phish_url_list.to_csv(filename, index=False, header=False)
+    #     phish_url_list = phish_url_list['url'].tolist()
+    # except requests.exceptions.RequestException as e:
+    #     raise SystemExit(e)
 
 
 if __name__ == "__main__":
-    loadDatasets()
+    fe.generate_legitimate_urls(15000)
 
-    for item in legitimate_url_list:
-        url = "http://" + item
-        print(url)
-        try:
-            r = requests.get(url)
-            print("[+]")
-        except requests.exceptions.RequestException as e:
-            print("[-]")
+    loadRawData()
+
+    print(legitimate_url_list)
